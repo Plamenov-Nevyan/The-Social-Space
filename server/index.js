25784c02b5f6a7bed882e65cec7538ecb8ff7f58
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-01-25 15:17:20 +0200

    Added socket connection for the server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,20 @@ const app = require('express')()
 const port = 8000
 const http = require('http').Server(app)
 const cors = require('cors')
+const socketIo = require("socket.io")(http, {
+    cors : {
+        origin : 'http://localhost:3000'
+    }
+})
 
 app.use(cors())
+
+socketIo.on('connection', (socket) => {
+    // code here.. 
+    socketIo.on('disconnect', () => {
+        // code here..
+    })
+})
 
 app.get('/', (req, res) => {
     res.json({
