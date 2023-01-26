aec0565cea12a54ba8ec3456920b09ee35e66e73
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-01-26 09:05:46 +0200

    Added appropriate stylization for chat header

--- client/src/components/Chat/ChatHeader/ChatHeader.js
+++ client/src/components/Chat/ChatHeader/ChatHeader.js
@@ -3,7 +3,8 @@ import styles from "./chatHeader.module.css"
 export function ChatHeader(){
     return (
         <div className={styles.container}>
-          <p>adasdasdasdasdasd</p>
+          <h2 className={styles.header}>Chat with friends and colleagues !</h2>
+          <button className={styles["leave-btn"]}>LEAVE CHAT</button>
         </div>
         )
 }
