e30533c47b6fe885a4887c4d2ab3e068accb7a7a
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-01-26 09:29:59 +0200

    Added appropriate stylization for chat footer

--- client/src/components/Chat/ChatFooter/ChatFooter.js
+++ client/src/components/Chat/ChatFooter/ChatFooter.js
@@ -3,7 +3,13 @@ import styles from "./chatFooter.module.css"
 export function ChatFooter(){
     return (
         <div className={styles.container}>
-          <p>adasdasdasdasdasd</p>
+          <textarea 
+          className={styles["message-input"]} 
+          id="messageInput" 
+          name="messageInput"
+          placeholder="Type your message here..."
+          ></textarea>
+          <button className={styles["send-btn"]}>SEND</button>
         </div>
         )
 }
