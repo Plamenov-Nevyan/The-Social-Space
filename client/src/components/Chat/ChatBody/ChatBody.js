b6620c77acaa73b267d8303b5eaf95909bda10ba
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-01-26 11:20:26 +0200

    Added appropriate stylization for chat body

--- client/src/components/Chat/ChatBody/ChatBody.js
+++ client/src/components/Chat/ChatBody/ChatBody.js
@@ -3,7 +3,50 @@ import styles from "./chatBody.module.css"
 export function ChatBody(){
     return (
         <div className={styles.container}>
-          <p>adasdasdasdasdasd</p>
+          <div className={styles["sent-message-container"]}>
+          <p className={styles["username-sent"]}>You</p>
+            <div className={styles["text-field-sent"]}>
+            <p className={styles.message}>
+               Hello !
+            </p>
+            </div>
+          </div>
+          <div className={styles["received-message-container"]}>
+          <p className={styles["username-received"]}>Username</p>
+             <div className={styles["text-field-received"]}>
+                 <p className={styles.message}>
+                    Hey, what's up!
+                 </p>
+             </div>
+          </div>
+          <div className={styles["sent-message-container"]}>
+          <p className={styles["username-sent"]}>You</p>
+            <div className={styles["text-field-sent"]}>
+            <p className={styles.message}>
+               Hello !  Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello !
+            </p>
+            </div>
+          </div>
+          <div className={styles["received-message-container"]}>
+          <p className={styles["username-received"]}>Username</p>
+             <div className={styles["text-field-received"]}>
+                 <p className={styles.message}>
+                   Are you fuckin crazy man ?????Are you fuckin crazy man ?????Are you fuckin crazy man ?????
+                 </p>
+             </div>
+          </div>
+          <div className={styles["sent-message-container"]}>
+          <p className={styles["username-sent"]}>You</p>
+            <div className={styles["text-field-sent"]}>
+            <p className={styles.message}>
+               Hello !  Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello ! Hello !
+            </p>
+            </div>
+          </div>
+          <div className={styles["message-typing"]}>
+          <p>Username is typing...</p>
         </div>
+        </div>
+        
         )
 }
